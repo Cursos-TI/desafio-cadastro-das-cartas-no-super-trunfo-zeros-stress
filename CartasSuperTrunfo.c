c09e5d5f21a12f138407a8b71930ed1d21d91066
zeros-stress <guigafc18@gmail.com>
2025-09-09 01:17:30 +0000

    criei o print f CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,46 @@
-#include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+   #include <stdio.h>
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // carta 1 florianópolis
+    int codigocidade = 48;
+    char letra = 'f';
+    char cidade[50] = "florianópolis";
+    int população = 587.486;
+    float area = 675.4;
+    float pib = 23.550;
+    int pontosturisticos = 30;
+
+    // carta 2 criciuma
+    int codigocidade = 42;
+    char letra = 'c';
+    char cidade[30] = "criciuma";
+    int população = 227.438;
+    float area = 235.6;
+    float pib = 10.1;
+    int pontosturisticos = 15;
+ 
+    printf("Digite sua cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite o codigocidade: \n");
+    scanf("%d", &codigocidade);
+
+    printf("Digite a letra: \n");
+    scanf("%s", &letra);
+
+    printf("Digite a população: \n");
+    scanf("%d", &população);
+
+    printf("Digite sua area: \n");
+    scanf("%f", &area);
+
+    printf("Digite o pib: \n");
+    scanf("%f", &pib);
+
+    prinf("Digite o pontoturistico: \n");
+    scanf("%d", &pontosturisticos);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
